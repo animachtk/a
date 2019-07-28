fbdb3eaedd75061a48dfa41fa7c0fea9873fff0b
animachtk <51044695+animachtk@users.noreply.github.com>
2019-07-29 00:57:52 +0400

    Update yo.js

--- yo.js
+++ yo.js
@@ -93,7 +93,7 @@ function yo(sel) {
     options.player = ((options.title && /трейлер|trailer|teaser/i.test(options.title)) || t)
         ? 'trailer'
         : (!options.player)
-            ? 'moonwalk,hdgo,kodik,videocdn,torrent'
+            ? 'moonwalk,hdgo,kodik,vk,torrent'
             : options.player;
 
     var bg = (options.bg && options.bg.replace(/[^0-9a-z]/ig, ''))
